23540529bdbd4f207c17278661f9cd74604bf7b0
atoroghi <49584324+atoroghi@users.noreply.github.com>
2026-01-07 17:05:12 +0000

    Deploying to gh-pages from @ atoroghi/atoroghi.github.io@b0b5e56de83c23b144e3f08e6990d4b1898a4b64 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,16 +433,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-defended-my-ph-d-thesis",
+          title: 'I defended my Ph.D. thesis.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-i-joined-the-d3m-lab-as-a-postdoctoral-researcher",
+          title: 'I joined the D3M lab as a postdoctoral researcher.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
